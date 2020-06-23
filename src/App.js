85e7ae501d21806a439524c36d4fa3678ba6130b
Welber Marques <welbih@gmail.com>
2020-06-23 07:12:55 -0300

    update correction

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ export default function App() {
                 style={styles.likeText}
                 testID={`repository-likes-${repository.id}`}
               >
-                {repository.likes} curtida(s)
+                {repository.likes} curtidas
               </Text>
             </View>
   
